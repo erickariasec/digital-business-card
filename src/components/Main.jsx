dad5d4897f4a6c97a0add16f9c9236aacd891065
erickariasec <erickarias1999@hotmail.com>
2022-08-28 23:39:06 -0500

    Digital Business Card, adding personal details (about and interests)

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import "./Main.css";
+
+function Main() {
+    return (
+        <section className="personal-details">
+            <article className="about">
+                <h3 className="subtitle">About</h3>
+                <p className="description">Passionate about discovering and implementing new technological tools to contribute and positively impact the world through learning and continuous improvement.</p>
+            </article>
+            <article className="interests">
+                <h3 className="subtitle">Interests</h3>
+                <p className="description">Develop web apps. Exercising. Reader. Sociable. Entrepreneur. Travel geek. Beatle fanatic. Coffee fanatic.</p>
+            </article>
+        </section>
+    );
+}
+
+export default Main;
